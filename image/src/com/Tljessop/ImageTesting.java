5352780b4aff447c95d4898ecf08f55a94a5b16c
Thomas Jessop <Tljessop@hotmail.com>
2019-02-16 01:55:46 -0800

    After much battling with maven and my ide trying to run a Javafx ImageViewer, I have for the moment decided that without maven is good enough for now. This simple app will run, and latter I will figure out how to make a maven version of this project run with Intellij

---
+++ image/src/com/Tljessop/ImageTesting.java
@@ -0,0 +1,31 @@
+package com.Tljessop;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class ImageTesting extends Application {
+
+
+    @Override
+    public void start(Stage primaryStage) {
+        Pane root = new Pane();
+
+        Image image = new Image("com/Tljessop/images/girl.png");
+       ImageView view = new ImageView(image);
+
+        root.getChildren().add(view);
+
+        Scene scene = new Scene(root,300,300);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}//Class ImageTesting
