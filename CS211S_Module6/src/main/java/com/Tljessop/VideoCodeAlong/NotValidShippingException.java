67533382cfe00fa299e53e21b89260be5c5b9c3f
Thomas Jessop <tljessop@hotmail.com>
2019-02-26 02:59:43 -0800

    small custom exception

---
+++ CS211S_Module6/src/main/java/com/Tljessop/VideoCodeAlong/NotValidShippingException.java
@@ -0,0 +1,9 @@
+package com.Tljessop.VideoCodeAlong;
+
+public class NotValidShippingException extends Exception {
+
+    public NotValidShippingException(){
+        super("Not a valid shipping speed");
+    }
+
+}//NotValidShippingException
