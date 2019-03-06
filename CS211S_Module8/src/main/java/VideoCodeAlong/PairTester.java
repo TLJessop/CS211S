740dfedcfd05f9cc0ccdf0b7a373f49c3416450c
Thomas Jessop <tljessop@hotmail.com>
2019-03-06 15:13:21 -0800

    Just a instanation demo

---
+++ CS211S_Module8/src/main/java/VideoCodeAlong/PairTester.java
@@ -0,0 +1,14 @@
+package VideoCodeAlong;
+
+public class PairTester {
+
+    public static void main(String[] args) {
+
+        Pair<Integer> nums = new Pair<>(3,4);
+        Pair<String> words = new Pair<>("hello", "world");
+
+        System.out.println(nums);
+        System.out.println(words);
+    }//main
+
+}//PairTester
