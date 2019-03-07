9a6df1156f9fb04fe106a61d5fc6f6a80803295c
Thomas Jessop <tljessop@hotmail.com>
2019-03-06 22:36:32 -0800

    m

---
+++ CS211S_Module8/src/main/java/VideoCodeAlong/Box.java
@@ -0,0 +1,42 @@
+package VideoCodeAlong;
+
+public class Box<T> {
+
+    private T item;
+
+    public Box(T item){
+        this.item = item;
+    }
+
+    public T getItem() {
+        return item;
+    }
+
+    public void setItem(T item) {
+        this.item = item;
+    }
+
+    @Override
+    public String toString(){
+        return "Box holding " + item.toString();
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if (obj == null) return false;
+        if (obj == this) return true;
+
+        if (obj instanceof Box<?>){
+            Box<?> otherBox = (Box<?>) obj;
+
+            if (this.item.equals(otherBox.item)){
+                return true;
+            }else {
+                return false;
+            }
+
+        } else {
+            return false;
+        }
+    }
+}//Box
