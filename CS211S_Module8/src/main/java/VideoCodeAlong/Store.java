db008b906e7974d075650d2dec2cd713521b09f3
Thomas Jessop <tljessop@hotmail.com>
2019-03-06 22:19:33 -0800

    Added a couple methods to play around with upper and lower bounded wildcards

--- CS211S_Module8/src/main/java/VideoCodeAlong/Store.java
+++ CS211S_Module8/src/main/java/VideoCodeAlong/Store.java
@@ -13,6 +13,9 @@ public class Store {
         FeaturedSpot<Item> featuredThing = new FeaturedSpot<>();
         featuredThing.featureItem(book1,15);
 
+        FeaturedSpot<Clothing> featuredClothing = new FeaturedSpot<>();
+        featuredClothing.featureItem(buttonUp,60);
+
         System.out.println(featuredThing+"\n");
 
         featuredThing.featureItem(pawShirt,40);
@@ -25,9 +28,34 @@ public class Store {
 
         System.out.println(featuredThing);
 
+        System.out.println();
 
+        printItem(featuredThing);
 
+        System.out.println();
 
+        printItem(featuredClothing);
+
+        System.out.println();
+        featureNewItem(featuredThing);
     }//main
+
+    //Upper bounded example
+    public static void printItem(FeaturedSpot<? extends Item> spot){
+        Item item = spot.getItem();
+        System.out.println(item);
+    }
+
+    //Lower bounded example
+    public static void featureNewItem(FeaturedSpot<? super Item> spot){
+        Book book = new Book("Java for Puppies", "My dog");
+        spot.featureItem(book,50);
+        System.out.println(spot.getItem()+"\n");
+
+        Shirt tShirt = new Shirt("A realy cool shirt", Clothing.Size.MEDIUM,Color.GREEN);
+        spot.featureItem(tShirt,40);
+        System.out.println(spot.getItem());
+
+    }
 
 }//Store
