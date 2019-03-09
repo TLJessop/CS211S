1675aca39e860a9d2376721326479712c9d6f8c3
Thomas Jessop <tljessop@hotmail.com>
2019-03-09 00:35:59 -0800

    added constructor, name getter, species count getter and a method to add a speices to the list of species

--- Arachnida/src/main/java/SpiderApp/Genus.java
+++ Arachnida/src/main/java/SpiderApp/Genus.java
@@ -1,5 +1,6 @@
 package SpiderApp;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Genus {
@@ -7,6 +8,20 @@ public class Genus {
     private String name;
     private List<Specice> speciceList;
 
+    public Genus(String name){
+        this.name = name;
+        speciceList = new ArrayList<>();
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public int getNumberOfSpecices(){
+        return speciceList.size();
+    }
+
+    public boolean addSpecies(Specice specice){
+        return speciceList.add(specice);
+    }
 }//Genus
