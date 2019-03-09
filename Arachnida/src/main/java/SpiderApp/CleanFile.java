86713ee5305ded5e381ec9a92f12b31c1f82ec2c
Thomas Jessop <tljessop@hotmail.com>
2019-03-08 23:54:56 -0800

    start point of file parsing

---
+++ Arachnida/src/main/java/SpiderApp/CleanFile.java
@@ -0,0 +1,51 @@
+package SpiderApp;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class CleanFile {
+
+    public static void main(String[] args){
+
+        try (FileReader fileReader = new FileReader(new File("/home/thomas/Codelanding/java/CS211S/Arachnida/src/main/resources/521.csv"))
+             ; PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(new File("cleaned521.csv"))))){
+
+            Scanner fileScanner = new Scanner(fileReader);
+
+            int lineCount = 0;
+
+            while (fileScanner.hasNext()){
+                String line = fileScanner.nextLine().replace('|',',');
+                char[] chars = line.toCharArray();
+                int commaCount =0;
+                for (int i = 0; i < chars.length ; i++) {
+                    if (chars[i]==',') commaCount++;
+                }
+                if (commaCount>=26) {
+                    System.out.println(commaCount + " commas " + line);
+                    lineCount++;
+                }
+            }
+
+            System.out.println(lineCount);
+
+
+
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}//CleanFile
