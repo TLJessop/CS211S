d99656334c6616ec94aaafb83daeaa2a2befea7b
Thomas Jessop <tljessop@hotmail.com>
2019-04-17 17:07:23 -0700

    Skeleton of my linked queue implementation

---
+++ CS111C_Module13/src/main/java/VideoCodeAlong/LinkedQueue.java
@@ -0,0 +1,95 @@
+package VideoCodeAlong;
+
+public class LinkedQueue<T> implements QueueInterface<T> {
+
+
+    /**
+     * Task: Adds a new entry to the back of the queue.
+     *
+     * @param newEntry an object to be added
+     */
+    @Override
+    public void enqueue(T newEntry) {
+
+    }
+
+    /**
+     * Task: Removes and returns the entry at the front of the queue.
+     *
+     * @return either the object at the front of the queue or, if the
+     * queue is empty before the operation, null
+     */
+    @Override
+    public T dequeue() {
+        return null;
+    }
+
+    /**
+     * Task: Retrieves the entry at the front of the queue.
+     *
+     * @return either the object at the front of the queue or, if the
+     * queue is empty, null
+     */
+    @Override
+    public T getFront() {
+        return null;
+    }
+
+    /**
+     * Task: Detects whether the queue is empty.
+     *
+     * @return true if the queue is empty, or false otherwise
+     */
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    /**
+     * Task: Removes all entries from the queue.
+     */
+    @Override
+    public void clear() {
+
+    }
+
+    //Doubly linked node private inner-class
+    private class Node{
+
+        //Instance data
+        private T data;
+        private Node before, next;
+
+        //Constructor
+        private Node(T entry, Node before, Node next){
+            this.data = entry;
+            this.before = before;
+            this.next = next;
+        }
+
+        //Getters and Setters
+        public T getData() {
+            return data;
+        }
+
+        public void setData(T data) {
+            this.data = data;
+        }
+
+        public Node getBefore() {
+            return before;
+        }
+
+        public void setBefore(Node before) {
+            this.before = before;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+    }
+}//LinkedQueue
