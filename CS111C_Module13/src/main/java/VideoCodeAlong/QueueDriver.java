99d97c5b08351d8f97abb0b4e096992059fbeb70
Thomas Jessop <tljessop@hotmail.com>
2019-04-22 19:28:18 -0700

    tested the array based queue. Test passed

--- CS111C_Module13/src/main/java/VideoCodeAlong/QueueDriver.java
+++ CS111C_Module13/src/main/java/VideoCodeAlong/QueueDriver.java
@@ -3,9 +3,39 @@ package VideoCodeAlong;
 public class QueueDriver {
 
     public static void main(String[] args) {
-        QueueInterface<Integer> numQueque = new CirularLinkedQueue<>();
+        QueueInterface<Integer> numQueque = new ArrayQueue<>();
 
         System.out.println(numQueque);
+
+        numQueque.enqueue(1);
+        numQueque.enqueue(2);
+        numQueque.enqueue(3);
+        numQueque.enqueue(4);
+        numQueque.enqueue(5);
+
+        numQueque.enqueue(1);
+        numQueque.enqueue(2);
+        numQueque.enqueue(3);
+        numQueque.enqueue(4);
+        numQueque.enqueue(5);
+
+        numQueque.enqueue(1);
+        numQueque.enqueue(2);
+        numQueque.enqueue(3);
+        numQueque.enqueue(4);
+        numQueque.enqueue(5);
+
+        numQueque.enqueue(1);
+        numQueque.enqueue(2);
+        numQueque.enqueue(3);
+        numQueque.enqueue(4);
+        numQueque.enqueue(5);
+
+        numQueque.enqueue(1);
+        numQueque.enqueue(2);
+        numQueque.enqueue(3);
+        numQueque.enqueue(4);
+        numQueque.enqueue(5);
 
         numQueque.enqueue(1);
         numQueque.enqueue(2);
@@ -23,6 +53,7 @@ public class QueueDriver {
         System.out.println(numQueque.dequeue());
         System.out.println(numQueque.dequeue());
         System.out.println(numQueque.dequeue());
+        numQueque.clear();
 
         System.out.println(numQueque);
 
