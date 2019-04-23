04db27d9b969ce65107b0ef18a44377f92e43d6e
Thomas Jessop <tljessop@hotmail.com>
2019-04-22 17:53:35 -0700

    testing the CircularLinkedQueue class. Testing passed

--- CS111C_Module13/src/main/java/VideoCodeAlong/QueueDriver.java
+++ CS111C_Module13/src/main/java/VideoCodeAlong/QueueDriver.java
@@ -3,6 +3,30 @@ package VideoCodeAlong;
 public class QueueDriver {
 
     public static void main(String[] args) {
+        QueueInterface<Integer> numQueque = new CirularLinkedQueue<>();
+
+        System.out.println(numQueque);
+
+        numQueque.enqueue(1);
+        numQueque.enqueue(2);
+        numQueque.enqueue(3);
+        numQueque.enqueue(4);
+        numQueque.enqueue(5);
+
+
+        System.out.println(numQueque);
+
+        System.out.println(numQueque.isEmpty());
+
+        System.out.println(numQueque.dequeue());
+        System.out.println(numQueque.dequeue());
+        System.out.println(numQueque.dequeue());
+        System.out.println(numQueque.dequeue());
+        System.out.println(numQueque.dequeue());
+
+        System.out.println(numQueque);
+
+        System.out.println(numQueque.isEmpty());
 
     }
 
