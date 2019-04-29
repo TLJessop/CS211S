a4d7993697f1775780bc244fdedfcae47bc6f6a1
Thomas Jessop <tljessop@hotmail.com>
2019-04-29 16:44:03 -0700

    added a data validator method

--- CS211SModule12/src/main/java/HomeWork/Committee.java
+++ CS211SModule12/src/main/java/HomeWork/Committee.java
@@ -12,13 +12,29 @@ public final class Committee {
 
     //Constructor
     public Committee (String name, int numMembers, Member committeeChair, LocalDate formationDate){
-
+        validArgs(name,numMembers,committeeChair,formationDate);
         this.name = name;
         this.numMembers = numMembers;
         this.committeeChair = committeeChair;
         this.formationDate = formationDate;
     }
 
+    //Validator
+    private void validArgs(String name, int numMembers, Member committeeChair, LocalDate formationDate){
+        if (name.isEmpty()){
+            throw new IllegalArgumentException("The name cannot be an empty string");
+        }
 
+        if (numMembers < 0){
+            throw new IllegalArgumentException("Number of members cannot be a negative number");
+        }
 
+        if (committeeChair != null){
+            throw new IllegalArgumentException("Committee chair cannot be null");
+        }
+
+        if (formationDate.isBefore(LocalDate.now().minusDays(30))){
+            throw new IllegalArgumentException("Formation date cannot be back dated over 30 days");
+        }
+    }
 }//Committee
