ff700c24d024c00500f81b8687f1077448fe8a32
Thomas Jessop <tljessop@hotmail.com>
2019-05-06 16:24:56 -0700

    wrote run method of the cook thread

--- CS211S_Module12/src/main/java/Homework/CookThread.java
+++ CS211S_Module12/src/main/java/Homework/CookThread.java
@@ -1,5 +1,6 @@
 package Homework;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
 
@@ -11,12 +12,32 @@ public class CookThread implements Runnable{
 
     //Constructor
     public CookThread(List<Food> thingsToMake, BlockingQueue<Food> ready){
-        this.thingsToMake = thingsToMake;
+        this.thingsToMake = new ArrayList<>(thingsToMake);
         this.ready = ready;
     }
 
+    @Override
     public void run() {
+        try {
+            while (!thingsToMake.isEmpty()){
+                Food currentFood = thingsToMake.get(0);
+                //if (ready.offer(currentFood))
+                if (true)
+                {
+                System.out.println("Cook starting " + currentFood);
+                Thread.sleep(currentFood.getCookTime() * 1000);
+                System.out.println("Cook finishing " + currentFood);
 
+                    thingsToMake.remove(currentFood);
+                } else {
+                    System.out.println("waiting for window space");
+                    continue;
+                }
+            }
+        } catch (InterruptedException e){
+            System.out.println("Cook thread interrupted");
+            return;
+        }
     }
 
 }//CookThread
