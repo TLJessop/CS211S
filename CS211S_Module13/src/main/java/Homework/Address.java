03eb4683adbd0035b8b32f560902b141c5cdf420
Thomas Jessop <tljessop@hotmail.com>
2019-05-13 16:56:33 -0700

    add annotation skelton and package statements

--- CS211S_Module13/src/main/java/Homework/Address.java
+++ CS211S_Module13/src/main/java/Homework/Address.java
@@ -1,3 +1,5 @@
+package Homework;
+
 import java.lang.annotation.*;
 import java.lang.reflect.*;
 
--- CS211S_Module13/src/main/java/Homework/AddressTest.java
+++ CS211S_Module13/src/main/java/Homework/AddressTest.java
@@ -1,3 +1,5 @@
+package Homework;
+
 public class AddressTest {
 
 	public static void main(String[] args) {
