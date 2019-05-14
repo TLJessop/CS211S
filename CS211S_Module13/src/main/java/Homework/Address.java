26e5c2684119ebdeb07c1e30a20c59e0e0a7b92a
Thomas Jessop <tljessop@hotmail.com>
2019-05-13 19:39:23 -0700

    implemented the validate lengths method and add proper length annotations

--- CS211S_Module13/src/main/java/Homework/Address.java
+++ CS211S_Module13/src/main/java/Homework/Address.java
@@ -4,11 +4,22 @@ import java.lang.annotation.*;
 import java.lang.reflect.*;
 
 public class Address {
-	
-	private String street;	
+
+
+	@ProperLength()
+	private String street;
+
 	private String street2;
+
+	@ProperLength(max_lendht = 40)
 	private String city;
+
+	@ProperLength(min_lenght = 2,
+			max_lendht = 2)
 	private String state;
+
+	@ProperLength(min_lenght = 5,
+				max_lendht = 5)
 	private String zip;
 
 	public Address(String street, String street2, String city, String state, String zip) throws IllegalArgumentException  {
@@ -20,9 +31,31 @@ public class Address {
 		
 		validateLengths();
 	}
-	
+
 	private void validateLengths() throws IllegalArgumentException {
-		// YOUR CODE HERE
+		Field[] fields = this.getClass().getDeclaredFields();
+		for (Field field: fields){
+			ProperLength  lengthAnnotation = field.getAnnotation(ProperLength.class);
+
+			if(lengthAnnotation != null){
+				String  testedField = null;
+				try {
+					testedField	= (String) field.get(this);
+				} catch (IllegalAccessException e) {
+					e.printStackTrace();
+				}
+
+				if (testedField.length() > lengthAnnotation.max_lendht()){
+					throw new IllegalArgumentException("IllegalArgumentException: Invalid address value [" +field.getName() +"=" + testedField + "] max length "
+							+ lengthAnnotation.max_lendht());
+				}
+				if (testedField.length() < lengthAnnotation.min_lenght()){
+					throw new IllegalArgumentException("IllegalArgumentException: Invalid address value [" +field.getName() +"=" + testedField + "] min length "
+							+ lengthAnnotation.min_lenght());
+				}
+
+			}
+		}
 	}
 	
 	@Override
