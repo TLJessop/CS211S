96fa055317b94651ba91406c24aef8cb8e672fbb
Thomas Jessop <tljessop@hotmail.com>
2019-05-13 17:11:04 -0700

    built annotation

--- CS211S_Module13/src/main/java/Homework/ProperLength.java
+++ CS211S_Module13/src/main/java/Homework/ProperLength.java
@@ -1,5 +1,14 @@
 package Homework;
 
-public @interface ProperLength {
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface ProperLength {
+    int min_lenght() default 1;
+    int max_lendht() default 255;
 }//ProperLength
